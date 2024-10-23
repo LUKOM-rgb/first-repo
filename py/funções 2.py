18cb8e1d2c6c1188f224e9d16f5657847b8ff227
LUKOM-rgb <40240224@esmad.ipp.pt>
2024-10-23 11:53:27 +0100

    olá jaquim

---
+++ py/funções 2.py
@@ -0,0 +1,26 @@
+def maior(numeros):
+
+    
+    maximo = 0
+    for i in range (0,numero):
+        if numero > i:
+            maximo = numero
+    return maximo
+
+     
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+print(maior(10,20,12))
+print(maior(5,15,20,14))
---
+++ py/funções1.py
@@ -0,0 +1,21 @@
+def somatorio(numero,numero2):
+
+    for i in range (numero,numero2):
+     soma+=i       
+    print(soma)    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+numero = input("Insira um numero entre 1 a 3: ")
+numero2 = input("insira um numero entre 3 a 6: ")
+somatorio(numero,numero2)
